cce3bd160aff77f874718c5e8510cfc7c7e2f5ab
NatsukazeRetro <NatsukazeRetro@gmail.com>
2018-05-21 20:42:14 +0900

    update

---
+++ common/mathjax/config.js
@@ -0,0 +1,13 @@
+MathJax.Hub.Config({
+    tex2jax: {
+	inlineMath: [['$','$'], ['\\(','\\)']],
+	processEscapes: true
+    },
+    TeX: {
+	equationNumbers: { autoNumber: "AMS" },
+    },
+    CommonHTML: {
+	scale: 110,
+	mtextFontInherit: true
+    }
+});
