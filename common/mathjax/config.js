3b11468e0792df09edb651a5dfdcaf01e96404d5
NatsukazeRetro <NatsukazeRetro@gmail.com>
2018-05-22 00:10:20 +0900

    update

--- common/mathjax/config.js
+++ common/mathjax/config.js
@@ -1,4 +1,4 @@
-window.MathJax = {
+MathJax.Hub.Config({
     tex2jax: {
 	inlineMath: [['$','$'], ['\\(','\\)']],
 	processEscapes: true
@@ -16,4 +16,6 @@ window.MathJax = {
 	scale: 110,
 	mtextFontInherit: true
     }
-};
+});
+
+MathJax.Ajax.loadComplete("https://natsukazeretro.githubio/common/mathjax/config.js");
