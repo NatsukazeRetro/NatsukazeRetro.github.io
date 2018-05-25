f1cf21a6bbe92eaa2029fac971781c6d19357000
NatsukazeRetro <NatsukazeRetro@gmail.com>
2018-05-25 20:56:06 +0900

    update

--- common/MathJax-2.7.4/config/local/local.js
+++ common/MathJax-2.7.4/config/local/local.js
@@ -34,11 +34,8 @@ MathJax.Hub.Register.StartupHook("TeX Jax Ready",function () {
   // place macros here.  E.g.:
   //   TEX.Macro("R","{\\bf R}");
   //   TEX.Macro("op","\\mathop{\\rm #1}",1); // a macro with 1 parameter
+  TEX.Macro("R","{\\mathbb R}");
   
-  TEX.Macro("C","{\\mathbb C}"); 
-  TEX.Macro("Q","{\\mathbb Q}"); 
-  TEX.Macro("R","{\\mathbb R}"); 
-  TEX.Macro("Z","{\\mathbb Z}"); 
 });
 
 MathJax.Ajax.loadComplete("[MathJax]/config/local/local.js");
